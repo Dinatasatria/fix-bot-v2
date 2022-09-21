c52dcd6d80f3240f2119aa24fe99a67e578494a5
Dinatasatria <111719054+Dinatasatria@users.noreply.github.com>
2022-09-21 19:45:10 +0800

    Update config.js

--- config.js
+++ config.js
@@ -37,19 +37,19 @@ global.sdc = '-' //discord
 global.snh = 'https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow' // my youtube channel
 
 /*============== PAYMENT ==============*/
-global.pdana = '08XXXXXXXXXX' //dana
+global . pdana   =   '087744410374'  //dana
 global.povo = '08XXXXXXXXXX' //ovo
-global.pgopay = '08XXXXXXXXXX' //gopay
+global.pgopay = '081353089407' //gopay
 global.plinkaja = '08XXXXXXXXXX' //link aja
-global.ppulsa = '08XXXXXXXXXX' //telkomsel
-global.ppulsa2 = '08XXXXXXXXXX' // kalau ada kartu lain isi aja
-global.psaweria = 'https://saweria.com/mrw4h1d'
+global . ppulsa    =    '081353089407'    //telkomsel
+global . ppulsa2   =   '087744410374'   // kalau ada kartu lain isi aja
+global.psaweria = 'https://saweria.com/Artasatriadi'
 
 /*============== NOMOR ==============*/
-global.nomorbot = '628XXXXXXXXXX'
-global.nomorown = '628XXXXXXXXXX'
-global.namebot = 'WH-MODS-BOT-V1'
-global.nameown = 'WH MODS DEV'
+global .nomorbot  =  '6281353089407'
+global . nomorown =  '6281353089407'
+global . nama     bot     =      'Arta SatriadiBOT-'
+global.nameown = 'Arta Satriadi'
 
 /*============== APIKEY SIMPEL ==============*/
 global.zenzkey = 'b8040941f7'
@@ -58,7 +58,7 @@ global.lolkey = '8e66d0934cf741bfd2182c16' // PUNYA WAHID 357d3c85079c4e0818731e
 
 /*============== STAFF ==============*/
 global.owner = [
-  ['628XXXXXXXXXX', 'WH MODS DEV', true],
+  ['628XXXXXXXXXX', 'WH MODS DEV', true]
   ['-', 'isi_kalau_ada', true],
   ['-', 'isi_kalau_ada', true],
   ['-', 'isi_kalau_ada', true] //Ganti jd nomormu sama Namamu
